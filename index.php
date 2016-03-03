f259a73e1064ca41e523dd26efb5dca800c83a41
1400003 <wld-ice@hotmail.com>
2016-03-03 16:08:14 +0000

    test upload

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>test</title>
+</head>
+<body>
+    <h1>We are testing now ADMINER! RAWR</h1>
+    <div>
+        <form action="upload.php" method="post" enctype="multipart/form-data">
+            Select image to upload:
+            <input type="file" name="fileToUpload" id="fileToUpload">
+            <input type="submit" value="Upload Image" name="submit">
+        </form>
+    </div>
+</body>
+</html>
