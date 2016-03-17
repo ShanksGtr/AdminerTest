1d8bbdcc5df95c1dea2599c88b9621e5c441328d
1400003 <wld-ice@hotmail.com>
2016-03-17 19:16:28 +0000

    getting pages test

--- index.php
+++ index.php
@@ -17,7 +17,7 @@
         <?php
         include_once('simple_html_dom.php');
         $html = file_get_html('http://www.videogamecountdown.com/');
-        foreach($html->find('img[$=5]') as $images)
+        foreach($html->find('div[img$=5]') as $images)
 
             echo '<img src="'.$images->src.'"/>' .  '<br>';
         //foreach($html->find('li') as $li)
