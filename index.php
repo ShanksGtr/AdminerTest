d2c914b530586ac74cf8f53b8bc71749703d904b
1400003 <wld-ice@hotmail.com>
2016-03-18 20:48:01 +0000

    getting pages test

--- index.php
+++ index.php
@@ -6,8 +6,8 @@
     <?php
         include('simple_html_dom.php');
         $html = file_get_html('http://www.videogamecountdown.com/');
-        foreach(array_slice($html->find('head script'),0 , 6) as $time){
-         echo $time->innertext;
+        foreach(array_slice($html->find('body script'),0 , 6) as $time){
+        $time->innertext;
     ?>
 
 </head>
