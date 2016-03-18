1393d1d90cff5f494df3d1e6e05f9bacc6b30cd2
1400003 <wld-ice@hotmail.com>
2016-03-18 18:04:43 +0000

    getting pages test

--- index.php
+++ index.php
@@ -28,9 +28,15 @@
 
         // http://stackoverflow.com/questions/8765879/how-to-limit-foreach-loop-to-three-loops/8765910#8765910
         foreach(array_slice($html->find('div[class=inner]'),0 , 6) as $class) {
-
             echo $class->innertext . "<br>";
             }
+        foreach(array_slice($html->find('div[class=inner]'),0 , 6) as $class) {
+            $games['image'] = $class->find('div.gridimg')->src;
+
+            $class[] = $games
+            }
+            echo $class;
+
         ?>
 
     </div>
