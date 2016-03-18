12d0db0cb3dc6a75d1683ded24d4b383008b92f5
1400003 <wld-ice@hotmail.com>
2016-03-18 21:23:18 +0000

    getting pages test

--- index.php
+++ index.php
@@ -37,7 +37,7 @@
 
               // http://stackoverflow.com/questions/8765879/how-to-limit-foreach-loop-to-three-loops/8765910#8765910
         foreach(array_slice($html->find('li div.inner'),0 , 6) as $class)
-            echo $class->innertext;
+            echo $class['img']->src;
 
            // $item['image'] = ($class->find('img')->src);
            // echo $class->href . "<br>".
