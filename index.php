0c3332d7d381d8e551e0959aed9c8de3319dd77f
1400003 <wld-ice@hotmail.com>
2016-03-18 20:31:53 +0000

    getting pages test

--- index.php
+++ index.php
@@ -35,7 +35,8 @@
 
               // http://stackoverflow.com/questions/8765879/how-to-limit-foreach-loop-to-three-loops/8765910#8765910
         foreach(array_slice($html->find('body'),0 , 6) as $class)
-            echo $class->innertext;
+            echo $class->href . "<br>" .
+             $class->date;
            // $item['image'] = ($class->find('img')->src);
            // echo $class->href . "<br>".
               //  $class->a . "<br>".
