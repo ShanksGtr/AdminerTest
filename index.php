19c8cdbe74594aac72d77db010a7a3a0f91cac64
1400003 <wld-ice@hotmail.com>
2016-03-18 21:59:02 +0000

    getting pages test

--- index.php
+++ index.php
@@ -39,7 +39,7 @@
         $games = $html->find('div[class=inner]');
         foreach (array_slice($games , 0,5) as $game) {
         //foreach(array_slice($html->find('div.inner'),0 , 6) as $class) {
-            $images = $game->find('div[class=gridimg] a',0 )->attr['href'];
+            $images = $game->find('div[class=gridimg] a')->attr['href'];
             $titles = $games->find('h3');
             echo $images . "<br>" . $titles;
         }
