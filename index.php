44b3ff1e1e24e7e97aca2fa9edef0c585b820dbe
Roronoa <wld-ice@hotamil.com>
2016-03-20 18:39:59 +0000

    03/20/2016

--- index.php
+++ index.php
@@ -34,15 +34,16 @@
 
 
         foreach (array_slice($games, 0,6) as $game) {
-            if ($game == false) {
 
-            }
         //foreach(array_slice($html->find('div.inner'),0 , 6) as $class) {
            // $titles = $games->find('div[class=inner] h3', 0)->attr['a'];
             $images = $game->find('div[class=gridimg] img',0)->attr['src'];
             $info = $game->find('div[class=gridimg] a',0)->attr['href'];
             $date = $game->find('div[class=date] span', 0)->outertext;
             //$titles = $games->find('h3', 0);
+            if ($info == false) {
+
+            }
             echo '<img src="'.$images.'"/>' . "<br>" .
                 '<a href="http://www.videogamecountdown.com/'.$info.'"> ->For more information</a>' . "<br>"
                 . $date;
