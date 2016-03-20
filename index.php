feb6d9719eb2d1a6c4ab941ebbde5cd8a962202a
Roronoa <wld-ice@hotamil.com>
2016-03-20 15:35:32 +0000

    03/20/2016

--- index.php
+++ index.php
@@ -14,6 +14,14 @@
         </form>
     </div>
     <div style="img: border-radius: 50%; height: 200px; width: 200px;">
+        <div hidden>
+            <?php
+            include('simple_html_dom.php');
+            $html = file_get_html('http://www.videogamecountdown.com/');
+            foreach($html->find('body') as $body)
+                echo $body->innertext;
+            ?>
+        </div>
         <?php
         include('simple_html_dom.php');
         $html = file_get_html('http://www.videogamecountdown.com/');
@@ -53,14 +61,7 @@
 
         ?>
         <h2>----------------------------------------</h2>
-        <div hidden>
-        <?php
-        include('simple_html_dom.php');
-        $html = file_get_html('http://www.videogamecountdown.com/');
-        foreach($html->find('body') as $body)
-            echo $body->innertext;
-        ?>
-        </div>
+
 
     </div>
 </body>
