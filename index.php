52f27762fd8021266dfaee3afe34d09c7b679177
1400003 <wld-ice@hotmail.com>
2016-03-22 14:56:48 +0000

    22/03/2016

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <title>test</title>
+    <link rel="stylesheet" type="text/css" href="test.css">
 </head>
 <body>
     <h1>We are testing now ADMINER! RAWR and taking info</h1>
@@ -41,12 +42,12 @@
             $info = $game->find('div[class=gridimg] a',0)->attr['href'];
             $date = $game->find('div[class=date] span', 0)->outertext;
             $html = file_get_html('http://www.videogamecountdown.com/'.$info);
-            $games2 = $html->find('div[class=two_third]');
+            $games2 = $html->find('div[class=two_third] p');
             //$titles = $game->find('h3', 0);
 
             echo "<h2>".$titles."</h2>" . "<br>" .  '<img src="'.$images.'"/>' . "<br>" .
                 '<a href="http://www.videogamecountdown.com/'.$info.'"> ->For more information</a>' . "<br>"
-                . $date . " " . $games2->p;
+                . $date . " " . $games2->plaintext;
         }
            // $item['image'] = ($class->find('img')->src);
            // echo $class->href . "<br>".
