602b4f918589c79e93b76948aff4682adf316f5a
1400003 <wld-ice@hotmail.com>
2016-03-31 17:43:17 +0100

    31/3/16

--- slider2.php
+++ slider2.php
@@ -172,8 +172,47 @@
         display:none;
     }
 
+    img {
+        width: 100%;
+    }
+
 </style>
 <body>
+<nav class="navbar navbar-inverse navbar-fixed-top">
+    <div class="container">
+        <!-- Brand and toggle get grouped for better mobile display -->
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="index.php">SGamers</a>
+        </div>
+
+        <!-- Collect the nav links, forms, and other content for toggling -->
+        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+            <ul class="nav navbar-nav">
+                <li><a href="#">Link <span class="sr-only">(current)</span></a></li>
+                <li><a href="#">Link</a></li>
+                <li class="dropdown">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">UpcomingGames<span class="caret"></span></a>
+                    <ul class="dropdown-menu">
+                        <li><a href="upcoming.php?plat=ps3">Playstation 3</a></li>
+                        <li><a href="upcoming.php?plat=ps4">Playstation 4</a></li>
+                        <li><a href="upcoming.php?plat=vita">Playstation Vita</a></li>
+                        <li role="separator" class="divider"></li>
+                        <li><a href="upcoming.php?plat=xbox">Xbox One</a></li>
+                        <li role="separator" class="divider"></li>
+                        <li><a href="upcoming.php?plat=wii">Wii U</a></li>
+                        <li><a href="upcoming.php?plat=3ds">3DS</a></li>
+                        <li role="separator" class="divider"></li>
+                        <li><a href="upcoming.php?plat=pc">PC</a></li>
+                    </ul>
+                </li>
+            </ul>
 <div class="container">
     <div class="jumbotron">
         <div id="slides">
