961c55bc59de341c3b678ec65bfaef5aebf10a1e
1400003 <wld-ice@hotmail.com>
2016-03-31 17:12:30 +0100

    31/3/16

--- slider2.php
+++ slider2.php
@@ -10,14 +10,6 @@
 
     <script src="http://code.jquery.com/jquery-latest.min.js"></script>
     <script src="jquery.slides.min.js"></script>
-    <script>
-        $(function(){
-            $("#slides").slidesjs({
-                width: 940,
-                height: 528
-            });
-        });
-    </script>
 </head>
 <style>
     .rslides {
