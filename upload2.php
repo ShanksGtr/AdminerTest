ddb5ff0a3f2d3a7ed6b0b9f305df251892269672
Roronoa <wld-ice@hotamil.com>
2016-04-06 01:07:07 +0100

    4/3/2016

--- upload2.php
+++ upload2.php
@@ -34,7 +34,7 @@
                 "be56106772d6a4", "33eeb80a", "SGamers" );
 
             if ($db->connect_error){
-                die('Connectfailed'['.$db->connect_error.']);
+                echo "Error Error Error";
             }
             ini_set('display_errors', 1);
             ini_set('display_startup_errors', 1);
